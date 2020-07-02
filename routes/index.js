c22f6631773021d8f6765477f3342a9500ea4cd2
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-07-02 21:22:17 +0700

    init 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('home');
 });
 
+router.get('/baidang', function(req, res, next) {
+    res.render('baidang');
+});
+
+router.get('/lienhe', function(req, res, next) {
+    res.render('lienhe');
+});
 module.exports = router;
